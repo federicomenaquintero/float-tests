53f246b8237ebb37ed0b6d8d5c29b40ff5ee91a7
Federico Mena Quintero <federico@gnome.org>
2017-06-14 14:52:41 -0500

    Print to_css_string() from the parsed token, as well as the binary bits

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,38 @@ extern crate cssparser;
 
 use std::str::FromStr;
 
-use ::cssparser::{Parser, ParserInput};
+use ::cssparser::{Parser, ParserInput, Token, ToCss};
 
 fn print_f32 (prefix: &str, val: f32) {
     let bits = val.to_bits ();
-    println! ("{} - value: {}, bits: 0x{:x}", prefix, val, bits);
+    println! ("{} - value: {}, bits: {b:b} (0x{b:x})", prefix, val, b = bits);
 }
 
 fn print_f64 (prefix: &str, val: f64) {
     let bits = val.to_bits ();
-    println! ("{} - value: {}, bits: 0x{:x}", prefix, val, bits);
+    println! ("{} - value: {}, bits: {b:b} (0x{b:x})", prefix, val, b = bits);
 }
 
 fn main () {
     let my_float = f32::from_str ("0.67").unwrap ();
     let my_double = f64::from_str ("0.67").unwrap ();
 
-    print_f32 ("f32", my_float);
+    print_f32 ("f32::from_str", my_float);
 
-    print_f64 ("f64", my_double);
+    print_f64 ("f64::from_str", my_double);
     
     let mut input = ParserInput::new ("0.67");
     let mut parser = Parser::new (&mut input);
-    let parsed_float = parser.expect_number ().unwrap ();
-    print_f32 ("parsed f32", parsed_float);
+
+    let token = parser.next ().unwrap ();
+    match token {
+        Token::Number (ref nv) => {
+            println! ("Token.to_css_string() = \"{}\"", token.to_css_string ());
+            print_f32 ("Token::Number(NumericValue.value)", nv.value);
+        }
+
+        _ => {
+            panic! ("expected a number");
+        }
+    }
 }
